8f1891a96899362113228b24fb0c097c3f2126a7
honghaoli <honghaoli2013@u.northwestern.edu>
2017-09-10 23:48:20 -0500

    035

---
+++ cpp/035_search_insert_position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        int mid, insert;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if ( nums[mid] > target) {
+                end = mid - 1;
+                insert = mid;
+                continue;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+                insert = mid + 1;
+                continue;
+            } else {
+                return mid;
+            }
+        }
+
+        return insert;
+    }
+};
