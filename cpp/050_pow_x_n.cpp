7fac45a1a8be2afaf97e1a43f03a74bb0e1bc8c9
honghaoli <lhhtsinghua@gmail.com>
2017-09-12 16:04:06 -0500

    050

---
+++ cpp/050_pow_x_n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if (n == -1)
+            return 1.0 / x;
+        double half = myPow(x, n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * myPow(x, n - (n/2) * 2);
+        }
+    }
+};
