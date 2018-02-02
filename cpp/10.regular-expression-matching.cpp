0818cea5188988340de664ee64f5f5afa1cb9df5
honghaoli <lhhtsinghua@gmail.com>
2018-02-02 14:42:41 -0600

    regular expression bottom up

--- cpp/10.regular-expression-matching.cpp
+++ cpp/10.regular-expression-matching.cpp
@@ -1,55 +1,89 @@
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         vector<vector<int>> dp = vector<vector<int>>(p.size() + 1, vector<int>(s.size() + 1, -1));
+//         return isMatch(s, p, 0, 0, dp);
+//     }
+
+
+// private:
+//     bool isMatch(string &s, string &p, int pi, int si, vector<vector<int>> &dp) {
+//         // found all
+//         if (pi == p.size() && si == s.size()) return true;
+//         // pattern found end, the string not, so cannot match.
+//         if (pi == p.size()) return false;
+
+//         // check dynamic programm memory
+//         if (dp[pi][si] == 0) return false;
+//         if (dp[pi][si] == 1) return true;
+
+//         // not visited before, calculate it.
+//         // if current char has wildcard number
+//         if (pi < p.size() - 1 && p[pi + 1] == '*') {
+//             // consider * = 0 char
+//             if (isMatch(s, p, pi + 2, si, dp)) {
+//                 dp[pi][si] = 1;
+//                 return true;
+//             }
+//             // * > 0
+//             for(; si < s.size() && matchChar(p[pi], s[si]); si++) {
+//                 if (isMatch(s, p, pi + 2, si + 1, dp)) {
+//                     dp[pi][si] = 1;
+//                     return true;
+//                 }
+//             }
+//             // not found match
+//             dp[pi][si] = 0;
+//             return false;
+//         } else {
+//             if (si == s.size()) return false;
+//             // only one char to compare
+//             if (matchChar(p[pi], s[si]))
+//                 return isMatch(s, p, pi + 1, si + 1, dp);
+//             else {
+//                 dp[pi][si] = 0;
+//                 return false;
+//             }
+//         }
+//     }
+
+//     bool inline matchChar(char &cp, char &cs) {
+//         if (cp == '.')
+//             return true;
+//         return cp == cs;
+//     }
+// };
+
+
+
+// DP from bottom to up
+// inspired by discussion
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp = vector<vector<int>>(p.size() + 1, vector<int>(s.size() + 1, -1));
-        return isMatch(s, p, 0, 0, dp);
-    }
+        vector<vector<bool>> dp(p.size() + 1, vector<bool>(s.size() + 1, false));      
+        dp[0][0] = true;
 
-
-private:
-    bool isMatch(string &s, string &p, int pi, int si, vector<vector<int>> &dp) {
-        // found all
-        if (pi == p.size() && si == s.size()) return true;
-        // pattern found end, the string not, so cannot match.
-        if (pi == p.size()) return false;
-
-        // check dynamic programm memory
-        if (dp[pi][si] == 0) return false;
-        if (dp[pi][si] == 1) return true;
-
-        // not visited before, calculate it.
-        // if current char has wildcard number
-        if (pi < p.size() - 1 && p[pi + 1] == '*') {
-            // consider * = 0 char
-            if (isMatch(s, p, pi + 2, si, dp)) {
-                dp[pi][si] = 1;
-                return true;
+        for (int pi = 1; pi <= p.size(); pi++) {
+            // initial for s = "" empty
+            if (p[pi - 1] == '*') {
+                dp[pi][0] = dp[pi - 2][0];
             }
-            // * > 0
-            for(; si < s.size() && matchChar(p[pi], s[si]); si++) {
-                if (isMatch(s, p, pi + 2, si + 1, dp)) {
-                    dp[pi][si] = 1;
-                    return true;
+            for (int si = 1; si <= s.size(); si++) {
+                if (match(p[pi - 1], s[si - 1])) {
+                    dp[pi][si] = dp[pi - 1][si - 1];
+                } else if (p[pi - 1] == '*') {
+                    dp[pi][si] = dp[pi - 2][si] || (match(p[pi - 2], s[si - 1]) && dp[pi][si - 1]);
                 }
             }
-            // not found match
-            dp[pi][si] = 0;
-            return false;
-        } else {
-            if (si == s.size()) return false;
-            // only one char to compare
-            if (matchChar(p[pi], s[si]))
-                return isMatch(s, p, pi + 1, si + 1, dp);
-            else {
-                dp[pi][si] = 0;
-                return false;
-            }
         }
+
+        return dp[p.size()][s.size()];
     }
 
-    bool inline matchChar(char &cp, char &cs) {
-        if (cp == '.')
-            return true;
-        return cp == cs;
+private:
+    bool inline match(char &p, char &c) {
+        if (p == '.') return true;
+        return p == c;
     }
 };
--- cpp/84.largest-rectangle-in-histogram.cpp
+++ cpp/84.largest-rectangle-in-histogram.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
+        int n = heights.size();
+        if (n < 1) return 0;
+
+        int max_area = 0;
+        int i = 0;
+        vector<int> stack;
+        while (i < n) {
+            if (stack.empty() || heights[i] > heights[stack.back()]) {
+                stack.push_back(i);
+                i++;
+            } else {
+                int top = stack.back();
+                stack.pop_back();
+                int area = heights[top] * (stack.empty()? i : i - stack.back() - 1);
+                max_area = max(area, max_area);
+            }
+        }
+
+        // then pop out all the remaining bars
+        while (!stack.empty()) {
+            int top = stack.back();
+            stack.pop_back();
+            int area = heights[top] * (stack.empty() ? i : i - stack.back() - 1);
+            max_area = max(area, max_area);
+        }
+
+        return max_area;
     }
 };
