a429f84efa40c513726f75449992fc5d7e9a628a
honghaoli <lhhtsinghua@gmail.com>
2018-02-16 16:15:35 -0600

    Morris traversal

--- cpp/144.binary-tree-preorder-traversal.cpp
+++ cpp/144.binary-tree-preorder-traversal.cpp
@@ -48,3 +48,42 @@ public:
         return result;
     }
 };
+
+
+
+// Morris traversal!!!!!
+class Solution {
+public:
+    // Morris traversal, only takes O(1) space.
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+        TreeNode *pre, *cur;
+        cur = root;
+        while (cur) {
+            // if left is nullptr, just use current and then move to right.
+            if (!cur->left) {
+                result.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                // left is not null, put next node as the right child of the right-most leaf of left subtree.
+                pre = cur->left;
+                while (pre->right && pre->right != cur) {
+                    pre = pre->right;
+                }
+                // found the right-most leaf of left subtree
+                if (!pre->right) {
+                    result.push_back(cur->val);
+                    pre->right = cur;
+                    cur = cur->left;        // start from left subtree
+                } else {
+                    // the right-most leaf is already current node, that means the left subtree is finished.
+                    // then change back to the original tree structure
+                    pre->right = nullptr;
+                    cur = cur->right;
+                }
+            }
+        }
+        return result;
+    }
+};
--- cpp/94.binary-tree-inorder-traversal.cpp
+++ cpp/94.binary-tree-inorder-traversal.cpp
@@ -9,7 +9,36 @@
  */
 class Solution {
 public:
+    // Morris traversal, only takes O(1) space.
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        vector<int> result;
+        if (!root) return result;
+        TreeNode *pre, *cur;
+        cur = root;
+        while (cur) {
+            // if left is nullptr, just use current and then move to right.
+            if (!cur->left) {
+                result.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                // left is not null, put current as the right child of the right-most leaf of left subtree.
+                pre = cur->left;
+                while (pre->right && pre->right != cur) {
+                    pre = pre->right;
+                }
+                // found the right-most leaf of left subtree
+                if (!pre->right) {
+                    pre->right = cur;
+                    cur = cur->left;        // start from left subtree
+                } else {
+                    // the right-most leaf is already current node, that means the left subtree is finished.
+                    // then change back to the original tree structure
+                    pre->right = nullptr;
+                    result.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        return result;
     }
 };
