065758dfff91140544994bc61b195917df27dcf9
honghaoli <lhhtsinghua@gmail.com>
2018-02-16 14:32:33 -0600

    144 use stack

--- cpp/144.binary-tree-preorder-traversal.cpp
+++ cpp/144.binary-tree-preorder-traversal.cpp
@@ -24,3 +24,27 @@ private:
         if (node->right) preorder(node->right, result);
     }
 };
+
+
+
+// use stack, iteratively
+class Solution {
+public:
+    // use stack
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+        stack<TreeNode *> st;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode *node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->right)
+                st.push(node->right);
+            if (node->left)
+                st.push(node->left);
+        }
+        return result;
+    }
+};
