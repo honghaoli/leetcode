04990a701880d46da41f817b9a4512ba74e24cde
honghaoli <honghaoli2013@u.northwestern.edu>
2018-02-21 11:07:04 -0600

    merge

--- cpp/023_merge_k_sorted_lists.cpp
+++ cpp/023_merge_k_sorted_lists.cpp
@@ -33,3 +33,48 @@ public:
         return dummy->next;
     }
 };
+
+
+
+
+
+// divide and conquer
+class Solution {
+public:
+    // either priority_queue or divide and conquer would gives O(nklogk) time, where n is the average length of each list, and k is the number of total lists.
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return mergeKLists(lists, 0, lists.size() - 1);
+    }
+    
+
+private:
+    ListNode* mergeKLists(vector<ListNode *> &lists, int start, int end) {
+        if (start == end) return lists[start];
+        if (start > end) return nullptr;
+        int mid = (end - start) / 2 + start;
+        ListNode *left = mergeKLists(lists, start, mid);
+        ListNode *right = mergeKLists(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+    
+
+    ListNode* mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode dummy(0);
+        ListNode *cur = &dummy;
+        while (l1 && l2) {
+            if (l1->val > l2->val) {
+                cur->next = l2;
+                l2 = l2->next;
+            } else {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            cur = cur->next;
+        }
+        if (l1) cur->next = l1;
+        else if (l2) cur->next = l2;
+        return dummy.next;
+    }
+};
--- cpp/088_merge_sorted_array.cpp
+++ cpp/088_merge_sorted_array.cpp
@@ -32,3 +32,34 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+// more concise
+// merge from back to front
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int cur = m + n - 1;
+        int i1 = m - 1, i2 = n - 1;
+        // merge from back to front
+        while (i1 >= 0 && i2 >= 0) {
+            if (nums1[i1] > nums2[i2]) {
+                nums1[cur--] = nums1[i1];
+                i1--;
+            } else {
+                nums1[cur--] = nums2[i2];
+                i2--;
+            }
+        }
+        // in case trailing nums2 left.
+        while (i2 >= 0) {
+            nums1[cur--] = nums2[i2];
+            i2--;
+        }
+    }
+};
