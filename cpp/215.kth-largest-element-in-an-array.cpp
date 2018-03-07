c0fd358c6b506067f33e3acfc12ef4dbe5d4c9d0
honghaoli <lhhtsinghua@gmail.com>
2018-03-07 16:29:59 -0600

    215-2 quick sort

--- cpp/215.kth-largest-element-in-an-array.cpp
+++ cpp/215.kth-largest-element-in-an-array.cpp
@@ -16,3 +16,52 @@ public:
         return minHeap.top();
     }
 };
+
+
+
+
+//
+class Solution {
+public:
+    // quick sort method, select a pivot, divide the array into smaller, equals, larger three part
+    // divide and conquer
+    int findKthLargest(vector<int>& nums, int k) {
+        // shuffle is important for quick sort!!
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(nums.begin(), nums.end(), g);
+        //
+        return divide(nums, 0, nums.size() - 1, k);
+    }
+    
+
+private:
+    int divide(vector<int> &nums, int start, int end, int k) {
+        int pivot = start;
+        int curr = start + 1, last = end;
+        int start_smaller = end + 1;
+        while (curr <= last) {
+            if (nums[curr] > nums[pivot]) {
+                swap(nums[curr], nums[pivot]);
+                pivot = curr;
+                curr++;
+            } else if (nums[curr] < nums[pivot]) {
+                swap(nums[curr], nums[last]);
+                start_smaller = last;
+                last--;
+            } else {
+                curr++;
+                start_smaller = curr;
+            }
+        }
+        
+        int size_larger = pivot - start;
+        int size_small_equal = start_smaller - start;
+        if (k <= size_larger)
+            return divide(nums, start, pivot - 1, k);
+        else if (k > size_small_equal) 
+            return divide(nums, start_smaller, end, k - size_small_equal);
+        else
+            return nums[pivot];
+    }
+};
