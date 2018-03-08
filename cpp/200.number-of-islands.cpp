4498be1bc7de9c6af6fe57a6624f9b42453fe93c
honghaoli <honghaoli2013@u.northwestern.edu>
2018-03-07 19:55:38 -0600

    200

--- cpp/200.number-of-islands.cpp
+++ cpp/200.number-of-islands.cpp
@@ -1,3 +1,4 @@
+// BFS
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -50,5 +51,43 @@ private:
                 visited[i][j + 1] = true;
             }            
         }
+    }
+};
+
+
+
+
+
+
+// DFS
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        if (n < 1) return 0;
+        int m = grid[0].size();
+        if (m < 1) return 0;
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+
+private:
+    void dfs(vector<vector<char>> &grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid.at(0).size() || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 };
