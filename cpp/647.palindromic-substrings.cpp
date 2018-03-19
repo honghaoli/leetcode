efdc35173ff83ad6e5dde116236962d657692829
honghaoli <lhhtsinghua@gmail.com>
2018-03-19 13:48:54 -0500

    647 palindromic

--- cpp/647.palindromic-substrings.cpp
+++ cpp/647.palindromic-substrings.cpp
@@ -3,36 +3,17 @@ public:
     int countSubstrings(string s) {
         int n = s.size();
         if (n <= 1) return n;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        return count(s, n, dp);
-    }
-
-private:
-    int isPalindromic(string &s, int start, int end, vector<vector<int>> &dp) {
-        if (start > end) return 1;
-        if (dp[start][end] >= 0) return dp[start][end];
-        if (start == end) {
-            dp[start][end] = 1;
-            return 1;
-        }
-        if (s[start] == s[end] && isPalindromic(s, start + 1, end - 1, dp)) {
-            dp[start][end] = 1;
-            return 1;
-        } else {
-            dp[start][end] = 0;
-            return 0;
-        }
-    }
-
-    int count(string &s, int n, vector<vector<int>> &dp) {
-        int total = 0;
-        for (int end = 0; end < n; end++) {
-            for (int start = 0; start <= end; start++) {
-                if (isPalindromic(s, start, end, dp) > 0)
-                    total++;
+        int result = 0;
+        for (int mid = 0; mid < n; mid++) {
+            // odd length
+            for (int i = 0; mid - i >= 0 && mid + i < n && s[mid - i] == s[mid + i]; i++) {
+                result++;
+            }
+            // even length
+            for (int i = 0; mid - i >= 0 && mid + i + 1 < n && s[mid - i] == s[mid + i + 1]; i++) {
+                result++;
             }
         }
-        return total;
+        return result;
     }
 };
