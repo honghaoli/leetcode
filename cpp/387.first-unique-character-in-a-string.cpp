5650f4f681683697939a53ad11481514b9da85a9
honghaoli <honghaoli2013@u.northwestern.edu>
2018-04-03 23:42:51 -0500

    387-

--- cpp/387.first-unique-character-in-a-string.cpp
+++ cpp/387.first-unique-character-in-a-string.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         const int N = 26;
-        vector<int> hash(N, 0);
-        for (auto &c : s) 
-            hash[c - 'a']++;
+        vector<vector<int>> hash(N, vector<int>{0, -1});
         for (int i = 0; i < s.size(); i++) {
-            if (hash[s[i] - 'a'] == 1)
-                return i;
+            hash[s[i] - 'a'][0]++;
+            hash[s[i] - 'a'][1] = i;
         }
-        return -1;
+        int idx = s.size();
+        for (int i = 0; i < N; i++) {
+            if (hash[i][0] == 1)
+                idx = min(idx, hash[i][1]);
+        }
+        if (idx == s.size())
+            return -1;
+        return idx;
     }
 };
+
