f84ed9e76841cf2b4d5f2c22ea811f30058f51cd
honghaoli <honghaoli2013@u.northwestern.edu>
2018-04-16 22:55:42 -0500

    160

--- cpp/160.intersection-of-two-linked-lists.cpp
+++ cpp/160.intersection-of-two-linked-lists.cpp
@@ -9,36 +9,12 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int n1 = size(headA);
-        int n2 = size(headB);
-        if (n1 >= n2) {
-            headA = move_ith(headA, n1 - n2);
-        } else {
-            headB = move_ith(headB, n2 - n1);
+        ListNode *a = headA;
+        ListNode *b = headB;
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
         }
-        while (headA && headB) {
-            if (headA == headB) return headA;
-            headA = headA->next;
-            headB = headB->next;
-        }
-        return nullptr;
-    }
-
-private:
-    int size(ListNode* node) {
-        ListNode *tmp = node;
-        int s = 0;
-        while (tmp) {
-            tmp = tmp->next;
-            s++;
-        }
-        return s;
-    }
-
-    ListNode* move_ith(ListNode *node, int i) {
-        while (i-- > 0) {
-            node = node->next;
-        }
-        return node;
+        return a;
     }
 };
