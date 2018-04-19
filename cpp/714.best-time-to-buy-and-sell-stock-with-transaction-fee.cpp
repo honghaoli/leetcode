1ba7bf2b858c33a16f87c5fb3c9ccd186f384333
honghaoli <honghaoli2013@u.northwestern.edu>
2018-04-18 22:47:41 -0500

    714

---
+++ cpp/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,20 @@
+// lol: this is the most important part to make your runtime beats others!
+// hahahhahaha
+static int x = []() { std::ios::sync_with_stdio(false); cin.tie(NULL); return 0; }();
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        // p0, profit with 0 stock at hand;
+        // p1, profit with 1 stock at hand.
+        int p0 = 0, p1 = INT_MIN;
+        for (int &p : prices) {
+            int tmp = p0;
+            p0 = max(p0, p1 + p);
+            p1 = max(p1, tmp - p - fee);
+        }
+        return p0;
+    }
+};
+
+
