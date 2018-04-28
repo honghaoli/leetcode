0bb858424070b11fac3b38c53cec5303f3d8e4d8
honghaoli <honghaoli2013@u.northwestern.edu>
2018-04-27 22:12:21 -0500

    543-

--- cpp/543.diameter-of-binary-tree.cpp
+++ cpp/543.diameter-of-binary-tree.cpp
@@ -1,4 +1,5 @@
 
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -11,22 +12,22 @@
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        traversal(root, diameter);
+        diameter = 0;
+        depth(root);
         return diameter;
     }
 
 private:
-    pair<int, int> traversal(TreeNode *node, int &diameter) {
-        if (!node) return {-1, -1};
-        auto l = traversal(node->left, diameter);
-        auto r = traversal(node->right, diameter);
-        int left = max(l.first, l.second) + 1;
-        int right = max(r.first, r.second) + 1;
-        diameter = max(diameter, left + right);
-        // cout << node->val << ": " << left << ", " << right << endl;
-        return {left, right};
+    int diameter = 0;
+    int depth(TreeNode *node) {
+        if (!node) return -1;
+        int left = depth(node->left);
+        int right = depth(node->right);
+        diameter = max(diameter, left + right + 2);
+        return max(left, right) + 1;
     }
 };
+
+
 
 
