0fdb7fa682267bb609b460b6fe5914db7f90c0eb
honghaoli <honghaoli2013@u.northwestern.edu>
2018-06-04 23:44:43 -0500

    422

---
+++ cpp/422.valid_word_square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int n = words.size();
+        if (n == 0) return true;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < words[i].size(); ++j) {
+                if (j >= n || i >= words[j].size() || words[i][j] != words[j][i])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
